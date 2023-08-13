fd3b0546d802f25b758ed126b75040b65fdf809d
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-08-13 23:32:49 +0000

    tuple addition

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
 
+# adds 2 tuples.
 def add_tuple(tuple_a=(), tuple_b=()):
-    if len(tuple_a) >= 2:
-        a, c = tuple_a[0], tuple_a[1]
-    elif len(tuple_a) == 1:
-        a, c = tuple_a[0], 0
-    elif len(tuple_a) == 0:
-        a, c = 0, 0
+    if len(tuple_a) < 2:
+        tuple_a += (0, 0)
+    if len(tuple_b) < 2:
+        tuple_b += (0, 0)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
 
-    if len(tuple_b) >= 2:
-        b, d = tuple_b[0], tuple_b[1]
-    elif len(tuple_b) == 1:
-        b, d = tuple_b[0], 0
-    elif len(tuple_b) == 0:
-        b, d = 0, 0
 
-    new_tuple = (a+b, c+d)
-    return(new_tuple)
+if __name__ == "__main__":
+    tuple_a = (1, 89)
+    tuple_b = (88, 11)
+    new_tuple = add_tuple(tuple_a, tuple_b)
+    print(new_tuple)
+
+    print(add_tuple(tuple_a, (1, )))
+    print(add_tuple(tuple_a, ()))
