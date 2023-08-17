95667ef4bb3ffce4ded877879f9984835002e78d
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-08-17 03:05:00 +0000

    task 13

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
-    # Check if the input list is empty.
     if not my_list:
         return 0
-    # Initiate the sum of weights.
-    weighted_score = 0
-    weighted_total = 0
-
-    # Iterate through each score-weight tuple in the list.
-    for score, weight in my_list:
-        weighted_score += score * weight
-        weighted_total += weight
-    return weighted_score / weighted_total
+    total_sum, total_weight = 0, 0
+    for i in range(len(my_list)):
+        product = my_list[i][0] * my_list[i][1]
+        total_sum += product
+        total_weight += my_list[i][1]
+    result = total_sum / total_weight
+    return result
 
