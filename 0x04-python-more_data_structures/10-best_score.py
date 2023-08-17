7ca1eeb51ba13ccab082a7d381c4c03519c002ab
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-08-17 02:38:27 +0000

    Romans to integer

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
 
-# returns a key with the biggest integer value.
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    # Check if the dictionary is empty
+    if not a_dictionary:
         return None
 
-    best = None
-    for key in a_dictionary:
-        if best is None:
-            best = key
-        if a_dictionary[key] > a_dictionary[best]:
-            best = key
+    # Initialize 'best_key' with the first key
+    best_key = list(a_dictionary.keys())[0]
+    # Initialize 'max_value' with the value of the first key
+    max = a_dictionary[best_key]
 
-    return best
-
-
-if __name__ == "__main__":
-    a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
-    best_key = best_score(a_dictionary)
-    print("Best score: {}".format(best_key))
-
-    best_key = best_score(None)
-    print("Best score: {}".format(best_key))
+    # Iterate through the dictionary to find the key with the highest value
+    for key, value in a_dictionary.items():
+        if value > max:
+            max = value
+            best_key = key
+    return best_key
