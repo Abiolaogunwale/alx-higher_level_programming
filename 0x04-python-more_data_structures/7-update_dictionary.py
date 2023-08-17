991de27df76451695c91b611a63d1878887842d4
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-08-17 02:35:12 +0000

    sort and update dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,26 +1,9 @@
 #!/usr/bin/python3
 
-#function that replaces or adds key/value in a dictionary.
 def update_dictionary(a_dictionary, key, value):
+
+    # Update or add the specified key with the given value.
     a_dictionary[key] = value
+    # Return the updated dictionary
     return a_dictionary
-
-
-if __name__ == "__main__":
-    print_sorted_dictionary = \
-        __import__('6-print_sorted_dictionary').print_sorted_dictionary
-
-    a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
-    new_dict = update_dictionary(a_dictionary, 'language', "Python")
-    print_sorted_dictionary(new_dict)
-    print("--")
-    print_sorted_dictionary(a_dictionary)
-
-    print("--")
-    print("--")
-
-    new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-    print_sorted_dictionary(new_dict)
-    print("--")
-    print_sorted_dictionary(a_dictionary)
 
