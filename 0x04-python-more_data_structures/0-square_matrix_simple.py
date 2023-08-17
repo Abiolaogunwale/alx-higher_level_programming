5734947aeddb550cda992fbdd2e209e1428b6084
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-08-17 01:38:41 +0000

    1-search_replace.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,4 +3,3 @@
 def square_matrix_simple(matrix=[]):
     # List comprehension to create a new matrix with elements squared
     return [[column**2 for column in row] for row in matrix]
-
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
 
-#function that replaces all occurrences of an element by another in a new list.
 def search_replace(my_list, search, replace):
-    new_list = my_list[:]
-    for i in range(len(my_list)):
-        if my_list[i] == search:
-            new_list[i] = replace
-    return new_list
-
-
-if __name__ == "__main__":
-    my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
-    new_list = search_replace(my_list, 2, 89)
-
-    print(new_list)
-    print(my_list)
+    # List comprehension to create a new list with replacements.
+    return [replace if x == search else x for x in my_list]
