adb60545462ab7f0efeef9cc3b1099fe79901a67
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-08-17 02:56:44 +0000

    9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def multiply_by_2(a_dictionary):
-    # Use dictionary comprehension to create a new dictionary
-    # with values multiplied by 2.
-    return {key: value * 2 for key, value in a_dictionary.items()}
+    new_dict = a_dictionary.copy()
+#    new_dict = dict(a_dictionary)
+    for key in new_dict:
+        new_dict[key] *= 2
+    return new_dict
 
