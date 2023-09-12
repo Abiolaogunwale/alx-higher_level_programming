ab5ba78955d1b02095b0d283a22e24ea41601515
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-09-12 04:49:41 -0700

    Look up

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" Write a function that returns the list. """
+
+
+def lookup(obj):
+    """ Returns the list of attributes and methods of an object """
+
+    return dir(obj)
+
