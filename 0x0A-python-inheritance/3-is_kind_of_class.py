0ba937aaef1482cef1f162108a53b7a802d589a5
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-09-12 05:34:06 -0700

    Same class or inherit from

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" Defines a function. """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Checks class instance
+    obj: an object.
+    a_class: a class.
+    Returns: Bool
+    """
+
+    return isinstance(obj, a_class)
