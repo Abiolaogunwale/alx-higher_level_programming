916ef39dcf936b27322583258b64c8f10192b393
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-09-12 06:29:22 -0700

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""" Defines a function """
+
+
+class BaseGeometry:
+    """ Defines a bass geometry class. """
+
+    def area(self):
+        """ Area of geometry. """
+
+        raise Exception("area() is not implemented")
