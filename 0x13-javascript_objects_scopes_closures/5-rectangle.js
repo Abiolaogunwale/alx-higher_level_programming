309d3b30511e69e63953229767431d791cf0dc64
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-11-14 15:10:01 -0800

    5-rectangle.js

--- 0x13-javascript_objects_scopes_closures/5-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/5-rectangle.js
@@ -1,20 +1,6 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-
-class Square extends Rectangle {
+module.exports = class Square extends require('./4-rectangle') {
   constructor (size) {
     super(size, size);
   }
-
-  charPrint (c) {
-    for (let i = 0; i < this.height; i++) {
-      const arr = [];
-      for (let j = 0; j < this.width; j++) {
-        arr.push(c || 'X');
-      }
-      console.log(`${arr.join('')}`);
-    }
-  }
-}
-
-module.exports = Square;
+};
