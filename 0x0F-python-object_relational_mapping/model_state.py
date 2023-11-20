7486a5d778d968bc7ca094a612f810acbd1ae42c
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-11-19 16:58:14 -0800

    All states via SQLAlchemy

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-"""
-Defines a state model that contain the class definition
- of a State and an instance Base = declarative_base()
-"""
-from lib2to3.pytree import Base
-from sre_parse import State
-from unicodedata import name
+"""6. First state model
+contains the class definition of a State and an instance
+Base = declarative_base():"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -13,17 +9,8 @@ Base = declarative_base()
 
 
 class State(Base):
-    """
-    inherits from Base Tips
-    links to the MySQL table states
-    class attribute id that represents a column
-     of an auto-generated, unique integer, can't
-      be null and is a primary key
-    class attribute name that represents a column
-     of a string with maximum 128 characters and
-      can't be null
+    """state class inherits from Base"""
+    __tablename__ = 'states'
 
-    """
-    __tablename__ = "states"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
