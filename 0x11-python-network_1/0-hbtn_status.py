f53c212786f5f931aabb4b5a43b09246dcd9e2a6
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-12-01 13:55:56 -0800

    What's my status? #0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""write a script that fetches https://alx-intranet.hbtn.io/status"""
+from urllib import request
+
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+    with request.urlopen(url) as res:
+        data = res.read()
+        parsed_data = data.decode('utf-8')
+        res_type = type(data)
+        print(f"Body response:\n\t- type: {res_type}\n\t\
+- content: {data}\n\t- utf8 content: {parsed_data}")
