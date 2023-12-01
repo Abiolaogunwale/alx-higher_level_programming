1b0dc004ebe4435cf62fef4da5ecf5e8520237af
Abiolaogunwale <ajayiabiola79@yahoo.com>
2023-12-01 15:24:19 -0800

    error codes

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-"""2. POST an email #0
-takes in a URL and an email, sends a POST request to the passed URL with the
-email as a parameter, and displays the body of the response
-(decoded in utf-8)"""
+"""
+takes in a URL, sends a request to the URL and displays
+the body of the response (decoded in utf-8).
+"""
+
 import sys
-from urllib import request, parse
+from urllib import request, error
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
-    params = {"email": email}
-    utf_params = parse.urlencode(params).encode('utf-8')
-    req = request.Request(url, data=utf_params, method='POST')
-    with request.urlopen(req) as res:
-        print(res.read().decode('utf-8'))
+
+    own_request = request.Request(url)
+    try:
+        with request.urlopen(own_request) as response:
+            print(response.read().decode("ascii"))
+    except error.HTTPError as e:
+        print("Error code: {}".format(e.code))
